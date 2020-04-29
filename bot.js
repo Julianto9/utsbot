8abb9d27df043c17db2a4767ce9c9bd961d0432d
tohadibrex <tohadibro@gmail.com>
2020-04-30 01:46:18 +0700

    Update bot.js
    update

--- bot.js
+++ bot.js
@@ -5,11 +5,10 @@ const client = new Discord.Client();
 client.on('ready', () => {
 
     console.log('I am ready!');
- });
- 
-client.user.setActivity('/uts', { type: 'PLAYING' })
+	client.user.setActivity('/uts', { type: 'PLAYING' })
   .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
   .catch(console.error);
+ });
 
 client.on('message', message => {
     if(message.author.bot === false) {
