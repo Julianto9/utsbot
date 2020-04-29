b2a29a1a46db883cb9f9f232c0a166e6fed4b635
tohadibrex <tohadibro@gmail.com>
2020-04-30 00:35:01 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,8 +21,9 @@ client.on('message', message => {
        }
 	 if (message.content === 'uts') {
 	message.channel.send({embed: {
-  color: 3447003,
-  description: "A very simple Embed!"
+  "title": "Jadwal UTS",
+      "description": "**Senin**\nSistem Operasi Dasar = 07.30 - 09.10\nBasis Data Lanjut = 13.30 - 15.10\n\n**Selasa**\nB.Ing = 07.30 - 09.10\nIMK = 13.30 - 15.10\n\n**Rabu**\nAljabar Matriks = 07.30 - 09.10\n\n**Kamis**\n-\n\n**Jumat**\nPBO = 07.30 - 09.10",
+      "color": 4437415
 }});}
 
 });
