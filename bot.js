9f1ede703081cf5a27f16017ffe9dff1d583c871
tohadibrex <tohadibro@gmail.com>
2020-04-30 00:41:07 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,19 +6,17 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 
-    console.log('I am ready!');
+    console.log('rede');
 
 });
 
  
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('**Senin** Sistem Operasi Dasar = 07.30 - 09.10 Basis Data Lanjut = 13.30 - 15.10');
+    if (message.content === '@CTI') {
+       message.reply('apa mek');
        }
-	 if (message.content === 'pipik') {
-       message.reply('**Senin** Sistem Operasi Dasar = 07.30 \n09.10 Basis Data Lanjut = 13.30 - 15.10');
-       }
+	 
 	 if (message.content === '/uts') {
 	message.channel.send({embed: {
   "title": "Jadwal UTS",
@@ -30,6 +28,6 @@ client.on('message', message => {
 
  
 
-// THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+
+client.login(process.env.BOT_TOKEN);
