122678a64cbd2aed568ee9c3de90d93d47292849
tohadibrex <tohadibro@gmail.com>
2020-04-30 00:35:14 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
 	 if (message.content === 'pipik') {
        message.reply('**Senin** Sistem Operasi Dasar = 07.30 \n09.10 Basis Data Lanjut = 13.30 - 15.10');
        }
-	 if (message.content === 'uts') {
+	 if (message.content === '/uts') {
 	message.channel.send({embed: {
   "title": "Jadwal UTS",
       "description": "**Senin**\nSistem Operasi Dasar = 07.30 - 09.10\nBasis Data Lanjut = 13.30 - 15.10\n\n**Selasa**\nB.Ing = 07.30 - 09.10\nIMK = 13.30 - 15.10\n\n**Rabu**\nAljabar Matriks = 07.30 - 09.10\n\n**Kamis**\n-\n\n**Jumat**\nPBO = 07.30 - 09.10",
