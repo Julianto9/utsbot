5e5e947916274c245924eb93b803c417c261952c
tohadibrex <tohadibro@gmail.com>
2020-04-30 08:57:41 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,12 +7,16 @@ client.on('ready', () => {
     console.log('I am ready!');
  });
 
+
+
 client.on('message', message => {
     if(message.author.bot === false) {
 	    if (message.content === 'wes') {
        		message.channel.send('wes');
        }
 }
+
+
 if (message.content === '/w') {message.channel.send('$w');}
 	 
 	 if (message.content === '/uts') {
@@ -20,7 +24,16 @@ if (message.content === '/w') {message.channel.send('$w');}
   "title": "Jadwal UTS",
       "description": "**Senin**\nSistem Operasi Dasar = 07.30 - 09.10\nBasis Data Lanjut = 13.30 - 15.10\n\n**Selasa**\nB.Ing = 07.30 - 09.10\nIMK = 13.30 - 15.10\n\n**Rabu**\nAljabar Matriks = 07.30 - 09.10\n\n**Kamis**\n-\n\n**Jumat**\nPBO = 07.30 - 09.10",
       "color": 4437415
-}});}
+									}});}
+									
+	if (message.content === '/help') {
+	message.channel.send({embed: {
+      "title": "Commands",
+      "description": "**/help** : tolng\n**/uts** : jadwal uts",
+      "color": 7506394,
+      "thumbnail": {
+        "url": "https://i1.sndcdn.com/avatars-000042341361-yma5yn-t500x500.jpg"
+      }}});}
 
 });
 
