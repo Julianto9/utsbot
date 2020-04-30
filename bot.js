f0d7ebe7e459f8410cc22246a8ca704dc1634293
tohadibrex <tohadibro@gmail.com>
2020-04-30 09:15:44 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,17 +17,17 @@ client.on('message', message => {
 }
 
 
-if (message.content === '/tolong') {message.channel.send('toloooooooooooooooooooooooooooooong');}
-if (message.content === '/so'){message.channel.send("absen so" + "@everyone");message.channel.send("absen so" + "@everyone");message.channel.send("absen so" + "@everyone");}
+if (message.content === '.tolong') {message.channel.send('toloooooooooooooooooooooooooooooong');}
+if (message.content === '.so'){message.channel.send("absen so" + "@everyone");message.channel.send("absen so" + "@everyone");message.channel.send("absen so" + "@everyone");}
 	 
-	 if (message.content === '/uts') {
+	 if (message.content === '.uts') {
 	message.channel.send({embed: {
   "title": "Jadwal UTS",
       "description": "**Senin**\nSistem Operasi Dasar = 07.30 - 09.10\nBasis Data Lanjut = 13.30 - 15.10\n\n**Selasa**\nB.Ing = 07.30 - 09.10\nIMK = 13.30 - 15.10\n\n**Rabu**\nAljabar Matriks = 07.30 - 09.10\n\n**Kamis**\n-\n\n**Jumat**\nPBO = 07.30 - 09.10",
       "color": 4437415
 									}});}
 									
-	if (message.content === '/help') {
+	if (message.content === '.help') {
 	message.channel.send({embed: {
       "title": "Commands",
       "description": "**/help** : tolng\n**/uts** : jadwal uts\n**/tolong** : tolong",
